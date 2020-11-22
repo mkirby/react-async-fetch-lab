51a740038a0744f4d4f0094682d337c786f90266
Matthew Kirby <matt.kirby@yahoo.com>
2020-11-22 12:16:52 -0500

    commit tree 75931e3241113714dad0d8478e405faaf03076f6

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import AstronautContainer from './Containers/AstronautContainer'
 
-function App() {
-  return <AstronautContainer />
+class App extends React.Component {
+
+  state = {
+    api: [],
+    people: []
+  }
+
+  componentDidMount() {
+    fetch(`http://api.open-notify.org/astros.json`)
+    .then(r=>r.json()).then(data=> this.setState({api: data, people: data.people}))
+  }
+  
+  render () {
+    return <AstronautContainer />
+  }
 }
 
 export default App
